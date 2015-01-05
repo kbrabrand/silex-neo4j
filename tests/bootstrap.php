8baadd270aa90f15ee56123a4cb337d467d87e0d
Kristoffer Brabrand <kristoffer@brabrand.no>
2015-01-05 20:26:44 +0100

    Removed an embarassing typo..

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,5 +13,3 @@ if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php'))
         'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
         'php composer.phar install'.PHP_EOL);
 }
-
-$loader->add('Tobiassjosten\Silex\Provider\Test', __DIR__);
