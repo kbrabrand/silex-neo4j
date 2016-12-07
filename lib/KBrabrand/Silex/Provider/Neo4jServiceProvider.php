85471deb686714b317c55dcb0fd72f72c3ed69b9
Gunter Solf <gunter.solf@gmail.com>
2016-12-07 10:33:11 +0100

    Updated to Silex Version 2

--- lib/KBrabrand/Silex/Provider/Neo4jServiceProvider.php
+++ lib/KBrabrand/Silex/Provider/Neo4jServiceProvider.php
@@ -2,23 +2,24 @@
 
 namespace KBrabrand\Silex\Provider;
 
-use Silex\Application,
-    Silex\ServiceProviderInterface;
+use Silex\Application;
+use Pimple\ServiceProviderInterface;
+use Pimple\Container;
 
 use Everyman\Neo4j\Client as Neo4jClient;
 
 class Neo4jServiceProvider implements ServiceProviderInterface {
 
-    public function register(Application $app) {
+    public function register(Container $app) {
         $app['neo4j.transport'] = 'localhost';
         $app['neo4j.port'] = 7474;
 
-        $app['neo4j'] = $app->share(function() use ($app) {
+        $app['neo4j'] = function() use ($app) {
             return new Neo4jClient(
                 $app['neo4j.transport'],
                 $app['neo4j.port']
             );
-        });
+        };
     }
 
     public function boot(Application $app) { }
